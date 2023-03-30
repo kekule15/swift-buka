4a7739cd67e8344b244f553c99b6b49a0a8c803a
Augustus <onyekachiaugustus111@gmail.com>
2023-03-30 12:38:52 +0100

    Initial Commit

---
+++ Buka/BukaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BukaApp.swift
+//  Buka
+//
+//  Created by Augustus Onyekachi on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BukaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
